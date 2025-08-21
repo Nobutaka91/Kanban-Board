8c007a1385d48f88d7a53036fba39e0d0fb240e4
NobutakaSuzuki <suzuki_n@opening-line.co.jp>
2025-08-21 15:45:47 +0900

    kanbanBoardにカラムの追加 / 削除を実装
    createNewColumnとdeleteColumnを実装

---
+++ kanban-board/src/components/KanbanBoard.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import PlusIcon from "../icons/PlusIcon"
+import type { Column, Id } from "../types";
+import ColumnContainer from "./ColumnContainer";
+
+const KanbanBoard = () => {
+  const [columns, setColumns] = useState<Column[]>([]);
+  // console.log(columns);
+
+  function generateId() {
+    /* generate random number between 0 and 10000 */
+    return Math.floor(Math.random() * 10001)
+  }
+
+  function createNewColumn() {
+    const columnToAdd: Column = {
+      id: generateId(),
+      title: `Column ${columns.length + 1}`,
+    };
+
+    setColumns([...columns, columnToAdd])
+  }
+
+  function deleteColumn(id: Id) {
+    const filteredColumns = columns.filter((col) => col.id !== id);
+    setColumns(filteredColumns);
+  }
+
+  return (
+    <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
+      <div className="mx-auto flex gap-4">
+        <div className="flex gap-4">
+          {columns.map((col) => (
+            <div>
+              <ColumnContainer 
+                key={col.id}
+                column={col}  
+                deleteColumn={deleteColumn} 
+              />
+            </div>
+          ))}
+        </div>
+        <button
+          onClick={() => {
+            createNewColumn();
+          }} 
+          className="h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor p-4 ring-rose-500 hover:ring-2 flex gap-2">
+          <PlusIcon />
+          Add Column
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default KanbanBoard
